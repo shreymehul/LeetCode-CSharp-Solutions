ae7ad82151619cb4eb94d4f99d6c950201b89d2c
shreymehul <shreymehul@gmail.com>
2022-09-14 19:46:20 +0530

    647

--- 647.CountSubstrings.cs
+++ 647.CountSubstrings.cs
@@ -7,36 +7,42 @@
 // A substring is a contiguous sequence of characters within the string.
 
 public class Solution {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine ("Hello Mono World");
-        count("abba");
-    }
     public int CountSubstrings(string s) {
-        
-    }
-    public static int count(string s){
-        bool[,] dp = new bool[s.Length,s.Length];
-
-        for(int i =0; i<s.Length; i++){
-            dp[i][i]=true;
+        bool[,] dp = new bool[s.Length, s.Length];
+        Console.WriteLine(dp.Length);
+        for (int i = 0; i < s.Length; i++)
+        {
+            dp[i,i] = true;
         }
 
-        for(int i =1; i<s.Length; i++){
-            if(s[i]==s[i-1]){
-                dp[i][i-1] = true;
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                dp[i,i - 1] = true;
             }
         }
 
-        for(int d=2;d<s.Length;d++){
-            int i = 0; int j=d;
-            while(j<s.Length){
-                if(s[i]==s[j] && dp[i+1][j-1])
-                    dp[i][j] = true;
+        for (int d = 2; d < s.Length; d++)
+        {
+            int i = 0; int j = d;
+            while (j < s.Length)
+            {
+                if (s[i] == s[j] && dp[j - 1,i+1])
+                    dp[j,i] = true;
                 i++;
                 j++;
             }
         }
-        
+        int count = 0;
+        for (int i = 0; i < dp.GetLength(0); i++)
+        {
+            for (int j = i; j < dp.GetLength(1); j++)
+            {
+                if(dp[j,i])
+                    count++;
+            }
+        }
+        return count;
     }
 }
