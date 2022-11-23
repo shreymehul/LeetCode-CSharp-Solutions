433cbc83ee58d999f6d6b80c2d35e225fcfb53db
shreymehul <shreymehul@gmail.com>
2022-11-23 20:39:30 +0530

    new soln

---
+++ 17.LetterCombinations.cs
@@ -0,0 +1,34 @@
+// 17. Letter Combinations of a Phone Number
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+// Example 1:
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// Example 2:
+// Input: digits = ""
+// Output: []
+// Example 3:
+// Input: digits = "2"
+// Output: ["a","b","c"]
+
+public class Solution {
+    string[] dialPad = new string[]
+        {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public IList<string> LetterCombinations(string digits) {
+        IList<string> result = new List<string>();
+        if(digits != ""){
+            AllCombination(result,digits,"", 0);
+        }
+        return result;  
+    }
+    public void AllCombination(IList<string> result,string digits,string s, int idx)    {
+        if(idx == digits.Length){
+            result.Add(new string(s));
+            return;
+        }
+        int num = digits[idx] - '0';
+        foreach(char c in dialPad[num]){
+            AllCombination(result,digits,s + c, idx+1);
+        }
+    }
+}
--- 36.IsValidSudoku.cs
+++ 36.IsValidSudoku.cs
@@ -65,3 +65,34 @@ public class Solution {
         return true;
     }
 }
+
+//HasSet
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        HashSet<int>[] row = new HashSet<int>[9]; 
+        HashSet<int>[] col = new HashSet<int>[9]; 
+        HashSet<int>[] box = new HashSet<int>[9]; 
+        for(int i = 0; i < 9; i++){
+            row[i] = new HashSet<int>();
+            col[i] = new HashSet<int>();
+            box[i] = new HashSet<int>();
+        }
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    int val = board[i][j] - '0';
+                    if(!row[i].Contains(val) && !col[j].Contains(val) 
+                      && !box[(i/3)*3 + (j/3)].Contains(val)) {
+                        row[i].Add(val);
+                        col[j].Add(val);
+                        box[(i/3)*3 + (j/3)].Add(val);
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
