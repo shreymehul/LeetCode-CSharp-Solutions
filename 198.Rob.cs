458bca44bfc3b646d57d6b59db2f3b4ef4e192b9
shreymehul <shreymehul@gmail.com>
2023-01-05 19:39:24 +0530

    new soln

--- 198.Rob.cs
+++ 198.Rob.cs
@@ -50,3 +50,23 @@ public class Solution {
         return Math.Max(nums[0],nums[1]);
     }
 }
+
+public class Solution {
+    public int Rob(int[] nums) {
+        int[] dp = new int[nums.Length];
+        Array.Fill(dp,-1);
+        if(nums.Length == 1)
+            return nums[0];
+        if(nums.Length == 2)
+            return Math.Max(nums[1],nums[0]);
+        if(nums.Length == 3)
+            return Math.Max(nums[1],nums[0]+nums[2]);
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[0]+nums[2];
+        for(int i = 3; i<nums.Length;i++){
+            dp[i] = nums[i] + Math.Max(dp[i-2],dp[i-3]);
+        }
+        return Math.Max(dp[nums.Length-1],dp[nums.Length-2]);
+    }
+}
