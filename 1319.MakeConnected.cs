c77c617bda51c4f5a7b7a709aea17773376f4e1f
shreymehul <shreymehul@gmail.com>
2025-02-09 19:19:26 +0530

    uodate

--- 1319.MakeConnected.cs
+++ 1319.MakeConnected.cs
@@ -26,6 +26,64 @@
 // Explanation: There are not enough cables.
 
 
+//Approch: Union-Find
+public class Solution {
+    public int MakeConnected(int n, int[][] connections) {
+        // To connect n nodes, we need at least n - 1 cables
+        if (connections.Length < n - 1) 
+            return -1;
+
+        int[] parent = new int[n];
+        int[] rank = new int[n];
+        
+        // Initialize the parent array for union-find
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        int components = n; // Initially, every node is its own component
+
+        // Connect components using union-find
+        foreach (var con in connections) {
+            if (Union(con[0], con[1], rank, parent)) {
+                components--; // Successfully merged two components
+            }
+        }
+
+        // Number of operations to connect components is components - 1
+        return components - 1;
+    }
+
+    private bool Union(int node1, int node2, int[] rank, int[] parent) {
+        int root1 = Find(node1, parent);
+        int root2 = Find(node2, parent);
+
+        // If they are already in the same set, no union is performed
+        if (root1 == root2) return false;
+
+        // Union by rank optimization
+        if (rank[root1] > rank[root2]) {
+            parent[root2] = root1;
+        } else if (rank[root2] > rank[root1]) {
+            parent[root1] = root2;
+        } else {
+            parent[root1] = root2;
+            rank[root2]++;
+        }
+
+        return true;
+    }
+
+    private int Find(int node, int[] parent) {
+        // Path compression optimization
+        if (parent[node] != node) {
+            parent[node] = Find(parent[node], parent);
+        }
+        return parent[node];
+    }
+}
+
+//Approch: DFS
 public class Solution {
     public bool[] Visited = new bool[100000];
     public int MakeConnected(int n, int[][] connections)
